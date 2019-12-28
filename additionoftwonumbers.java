7a4e20d29c5de39bd0d42933191359bb7efc9f97
170031487 <59306302+170031487@users.noreply.github.com>
2019-12-28 15:04:11 +0530

    Add files via upload

---
+++ additionoftwonumbers.java
@@ -0,0 +1,9 @@
+import.java.util.Scanner;
+public class additionoftwonumbers{
+public static void main(String[]args)
+{
+int num1=5 , num2=6 , sum;
+sum=num1 + sum2;
+System.out.println("Sum of these numbers: "+sum);
+}
+}
